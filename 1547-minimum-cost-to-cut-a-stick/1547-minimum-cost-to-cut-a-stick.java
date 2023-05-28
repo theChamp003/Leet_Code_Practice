a52d0e0d09173b9410e26c5c03ba9ffef3372293
theChamp003 <94452677+theChamp003@users.noreply.github.com>
2023-05-29 02:58:44 +0530

    Time: 814 ms (5.09%), Space: 47.6 MB (6.67%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,30 @@
+class Solution {
+    HashMap<String,Integer> hm = new HashMap<>() ;
+    public int minCost(int n, int[] cuts) 
+    {
+       Arrays.sort(cuts);
+        return f(cuts,0,n);
+    }
+
+    public int f(int cuts[],int l,int r)
+    {
+       // if(l<0 || r > cuts.length)
+       // return 0;
+       String s = l + "kk" + r;
+       if(hm.containsKey(s))
+       return hm.get(s);
+       //String s = l + "kk" + r;
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<cuts.length;i++)
+        {
+            if(cuts[i]<=l || cuts[i]>=r)
+            continue;
+            res = Math.min(f(cuts,l,cuts[i])+r-l+f(cuts,cuts[i],r),res);
+
+        }
+        res = res == Integer.MAX_VALUE ? 0 : res;
+        hm.put(s,res);
+        return res;
+    }
+
+}
